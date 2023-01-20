26ec123b4e5b1cddba57b8fd2809be0bb3cff2d6
김현규 <67453494+kimm240@users.noreply.github.com>
2023-01-20 16:36:50 +0900

    Add OptimisticLock

--- backend/demo/demo/src/main/java/com/example/demo/persistence/TodoRepository.java
+++ backend/demo/demo/src/main/java/com/example/demo/persistence/TodoRepository.java
@@ -15,6 +15,9 @@ import java.util.List;
 public interface TodoRepository extends JpaRepository<TodoEntity, String>{
 	List<TodoEntity> findByUserId(String userId);
 	
+	@Lock(value = LockModeType.OPTIMISTIC)
+	List<TodoEntity> findByUserIdWithOptimisticLock(String userId);
+	
 	@Lock(value = LockModeType.PESSIMISTIC_WRITE)
     	List<TodoEntity> findByUserIdWithPessimisticLock(String userId);
 }
