3d9803d32998c734ee6fcfdfe12cbf2f392a7c09
김현규 <67453494+kimm240@users.noreply.github.com>
2023-01-20 13:26:33 +0900

    Add PessimisticLock

--- backend/demo/demo/src/main/java/com/example/demo/persistence/TodoRepository.java
+++ backend/demo/demo/src/main/java/com/example/demo/persistence/TodoRepository.java
@@ -2,11 +2,19 @@ package com.example.demo.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Lock;
+
+import javax.persistence.LockModeType;
+
 import com.example.demo.model.TodoEntity;
+
 
 import java.util.List;
 
 @Repository
 public interface TodoRepository extends JpaRepository<TodoEntity, String>{
 	List<TodoEntity> findByUserId(String userId);
+	
+	@Lock(value = LockModeType.PESSIMISTIC_WRITE)
+    	List<TodoEntity> findByUserIdWithPessimisticLock(String id);
 }
