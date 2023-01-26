7c81af829ee4f6b65dadf0fc7369ace59c316114
김현규 <67453494+kimm240@users.noreply.github.com>
2023-01-26 17:43:16 +0900

    Update TodoRepository.java

--- backend/demo/demo/src/main/java/com/example/demo/persistence/TodoRepository.java
+++ backend/demo/demo/src/main/java/com/example/demo/persistence/TodoRepository.java
@@ -1,14 +1,12 @@
 package com.example.demo.persistence;
 
+import com.example.demo.model.TodoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.query;
 
 import javax.persistence.LockModeType;
-
-import com.example.demo.model.TodoEntity;
-
-
 import java.util.List;
 
 @Repository
@@ -16,6 +14,7 @@ public interface TodoRepository extends JpaRepository<TodoEntity, String>{
 	List<TodoEntity> findByUserId(String userId);
 	
 	@Lock(value = LockModeType.OPTIMISTIC)
+	@Query("select * from Todo t where t.userId = ?1")
 	List<TodoEntity> findByUserIdWithOptimisticLock(String userId);
 	
 	//@Lock(value = LockModeType.PESSIMISTIC_WRITE)
